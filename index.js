2bdc4506fd4ff2034ae329b22f6d6f8211ba6760
Johan Smal <johan@babylonstoren.com>
2019-11-20 16:00:08 +0200

    next js

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
-const { sequelize, Team, Match, MatchResult } = require('./db/database');
+const express = require('express');
+const next = require('next');
 
-Team.findAll()
-	.then(teams => {
-		teams.map(team => {
-			console.log(team.name);
+const dev = process.env.NODE_ENV !== 'production';
+const port = dev ? 3000 : 80;
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare()
+	.then(() => {
+		const server = express();
+
+		server.get('*', (req, res) => {
+			return handle(req, res)
 		})
+
+		server.listen(port, (err) => {
+			if (err) throw err
+			console.log(`> Ready on http://localhost:${port}`);
+		})
+	})
+	.catch((ex) => {
+		console.error(ex.stack)
+		process.exit(1);
 	})
